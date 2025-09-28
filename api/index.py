14cb583570bc0be88fe08ca03156f8eeeb9caea9
mskim8717 <ms.kim8717@gmail.com>
2025-09-28 23:03:20 +0900

    change files

--- api/index.py
+++ api/index.py
@@ -1,16 +1,11 @@
-# api/index.py
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"Hello": "World"}
 
-@app.get("/api/test")
-def test():
-    return {"status": "working"}
-
-# Vercel 핸들러
-from mangum import Mangum
-handler = Mangum(app, lifespan="off")
+@app.get("/api/hello")
+def hello():
+    return {"message": "Hello from FastAPI"}

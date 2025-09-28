7079ce5d06b2a6b0e6cdc8db5742d11fe9d9d5e8
mskim8717 <ms.kim8717@gmail.com>
2025-09-28 23:10:40 +0900

    update all files

--- api/index.py
+++ api/index.py
@@ -3,9 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/api/hello")
-def hello():
-    return {"message": "Hello from FastAPI"}
+async def root():
+    return {"message": "Hello World"}
